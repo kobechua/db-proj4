f76449829d22adbb9e497a294f2177fba3b04a58
Kobe <kobechua14@gmail.com>
2023-04-08 19:55:29 -0400

    added server info and routes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-
-app.get("/getLawyers", (req, res) => {
-    res.json({
-        status: "success",
-        lawyer: "John Smith"
+app.use((req, res, next) => {
+    res.status(404).json({
+        "status": "failed"
     });
 
+});
+
+app.get("/api/ver1/lawyer", (req, res) => {
+    res.json({
+        status: "success",
+        data: {
+            lawyer: ["John Smith", "Jane Doe"]
+        }
+        
+    });
+
+});
+
+app.get("/api/ver1/lawyer/:id", (req, res) => {
+    console.log(req.params); 
+});
+
+app.post("/api/ver1/lawyer", (req, res) => {
+    console.log(req);
 });
 
 const port = process.env.PORT || 3000;
